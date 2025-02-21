54f0ac252e880a06ec3efbf194ea12de476a0442
joeel561 <nicole.wolf@goltfisch.de>
2025-02-21 16:40:32 +0100

    init

---
+++ game.go
@@ -0,0 +1,83 @@
+package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+const (
+	screenWidth  = 1000
+	screenHeight = 480
+)
+
+var (
+	running = true
+	bgColor = rl.NewColor(147, 211, 196, 255)
+
+	grassSprite  rl.Texture2D
+	playerSprite rl.Texture2D
+
+	playerSrc   rl.Rectangle
+	playerDest  rl.Rectangle
+	playerSpeed float32 = 3
+)
+
+func drawScene() {
+	rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
+}
+
+func input() {
+	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
+		playerDest.Y -= playerSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
+		playerDest.Y += playerSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
+		playerDest.X -= playerSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
+		playerDest.X += playerSpeed
+	}
+}
+func update() {
+	running = !rl.WindowShouldClose()
+}
+func render() {
+	rl.BeginDrawing()
+	rl.ClearBackground(bgColor)
+
+	drawScene()
+
+	rl.EndDrawing()
+}
+
+func init() {
+	rl.InitWindow(screenWidth, screenHeight, "godew valley - a game by joeel56")
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(60)
+
+	grassSprite = rl.LoadTexture("res/Tilesets/ground-tiles/New-tiles/Grass_tiles_v2.png")
+	playerSprite = rl.LoadTexture("res/Characters/CharakterSpritesheet.png")
+
+	playerSrc = rl.NewRectangle(0, 0, 32, 32)
+	playerDest = rl.NewRectangle(200, 200, 100, 100)
+
+}
+func quit() {
+	rl.UnloadTexture(grassSprite)
+	rl.UnloadTexture(playerSprite)
+	rl.CloseWindow()
+}
+
+func main() {
+
+	for running {
+		input()
+		update()
+		render()
+	}
+
+	quit()
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module main
+
+go 1.24.0
+
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20250215042252-db8e47f0e5c5
+
+require (
+	github.com/ebitengine/purego v0.8.2 // indirect
+	golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
