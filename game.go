01489ec3f3b87df2474c10ee87fbd133fb145029
joeel561 <nicole.wolf@goltfisch.de>
2025-02-25 17:27:22 +0100

    add bg music & sprint animation & idle animation

--- game.go
+++ game.go
@@ -14,9 +14,21 @@ var (
 	grassSprite  rl.Texture2D
 	playerSprite rl.Texture2D
 
-	playerSrc   rl.Rectangle
-	playerDest  rl.Rectangle
+	playerSrc                                     rl.Rectangle
+	playerDest                                    rl.Rectangle
+	playerMoving                                  bool
+	playerDir                                     int
+	playerUp, playerDown, playerLeft, playerRight bool
+	playerFrame                                   int
+
+	frameCount int
+
 	playerSpeed float32 = 3
+
+	musicPaused bool
+	music       rl.Music
+
+	cam rl.Camera2D
 )
 
 func drawScene() {
@@ -26,30 +38,115 @@ func drawScene() {
 
 func input() {
 	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
-		playerDest.Y -= playerSpeed
+		playerMoving = true
+		playerDir = 5
+		playerUp = true
 	}
 
 	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
-		playerDest.Y += playerSpeed
+		playerMoving = true
+		playerDir = 4
+		playerDown = true
 	}
 
 	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
-		playerDest.X -= playerSpeed
+		playerMoving = true
+		playerDir = 7
+		playerLeft = true
 	}
 
 	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
-		playerDest.X += playerSpeed
+		playerMoving = true
+		playerDir = 6
+		playerRight = true
+	}
+
+	if rl.IsKeyDown(rl.KeyLeftShift) || rl.IsKeyDown(rl.KeyRightShift) {
+		playerSpeed = 6
+	} else {
+		playerSpeed = 3
+	}
+
+	if rl.IsKeyPressed(rl.KeyQ) {
+		musicPaused = !musicPaused
 	}
 }
 func update() {
 	running = !rl.WindowShouldClose()
+
+	playerSrc.X = playerSrc.Width * float32(playerFrame)
+
+	if playerMoving {
+		if playerUp {
+			playerDest.Y -= playerSpeed
+
+			if playerSpeed == 6 {
+				playerDir = 9
+			}
+		}
+		if playerDown {
+			playerDest.Y += playerSpeed
+
+			if playerSpeed == 6 {
+				playerDir = 8
+			}
+		}
+		if playerLeft {
+			playerDest.X -= playerSpeed
+
+			if playerSpeed == 6 {
+				playerDir = 11
+			}
+
+		}
+		if playerRight {
+			playerDest.X += playerSpeed
+
+			if playerSpeed == 6 {
+				playerDir = 10
+			}
+		}
+
+		if frameCount%8 == 1 {
+			playerFrame++
+		}
+
+	} else if frameCount%45 == 1 {
+		playerFrame++
+
+	}
+
+	frameCount++
+	if playerFrame >= 8 {
+		playerFrame = 0
+	}
+
+	if !playerMoving && playerFrame > 1 {
+		playerFrame = 0
+	}
+	playerSrc.Y = playerSrc.Height * float32(playerDir)
+	playerSrc.X = playerSrc.Width * float32(playerFrame)
+
+	rl.UpdateMusicStream(music)
+	if musicPaused {
+		rl.PauseMusicStream(music)
+	} else {
+		rl.ResumeMusicStream(music)
+	}
+
+	cam.Target = rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2)))
+
+	playerMoving = false
+	playerUp, playerDown, playerLeft, playerRight = false, false, false, false
 }
+
 func render() {
 	rl.BeginDrawing()
 	rl.ClearBackground(bgColor)
+	rl.BeginMode2D(cam)
 
 	drawScene()
-
+	rl.EndMode2D()
 	rl.EndDrawing()
 }
 
@@ -61,13 +158,24 @@ func init() {
 	grassSprite = rl.LoadTexture("res/Tilesets/ground-tiles/New-tiles/Grass_tiles_v2.png")
 	playerSprite = rl.LoadTexture("res/Characters/CharakterSpritesheet.png")
 
-	playerSrc = rl.NewRectangle(0, 0, 32, 32)
+	playerSrc = rl.NewRectangle(0, 0, 48, 48)
 	playerDest = rl.NewRectangle(200, 200, 100, 100)
+
+	rl.InitAudioDevice()
+	music = rl.LoadMusicStream("res/bgmusic.mp3")
+
+	musicPaused = false
+	rl.PlayMusicStream(music)
+
+	cam = rl.NewCamera2D(rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)),
+		rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2))), 0, 1.5)
 
 }
 func quit() {
 	rl.UnloadTexture(grassSprite)
 	rl.UnloadTexture(playerSprite)
+	rl.UnloadMusicStream(music)
+	rl.CloseAudioDevice()
 	rl.CloseWindow()
 }
 
